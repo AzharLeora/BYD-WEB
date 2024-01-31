8c0f84f25ed96c3a2ad34406db7e9e23f45b1f0a
Vishva <prvishva24@gmail.com>
2024-01-31 18:39:47 +0530

    timeline code

--- src/Components/MyComponents/WeddingTimeLine/index.tsx
+++ src/Components/MyComponents/WeddingTimeLine/index.tsx
@@ -4,16 +4,14 @@ import { image } from '@Assets';
 const WeddingTimeLine=()=>{
     return(
         <>
-  
-        <div className=" container ">
+        <div className="timeline-bg">
+
+        <div className=" container  ">
            <div className="row">
             <div className="col-12">
   
               {/* title */}
               <div className="title-section row">
-                {/* <div className="col-2 col-md-3 pr-0"><hr></hr></div>
-                <div className="col-8 col-md-6 d-flex align-items-center justify-content-center text-center"><div className='title'>Our Dream Day's Timeline</div></div>
-                <div className="col-2 col-md-3 pl-0"><hr></hr></div> */}
   
                 <div className="col-2 col-md-3 pr-0">
                   <div className='d-none d-lg-block'>
@@ -62,7 +60,7 @@ const WeddingTimeLine=()=>{
                 <div className="spacing">
                 <div className="event">Reception</div>
                 <div className="timing mb-4">STARTS AT 06:30 PM</div>
-                <div className="Quote text-center text-md-left">"Its our pleasure to invite you all to be part of our special day"</div>
+                <div className="Quote text-center text-md-left">"Its our pleasure to invite you all to be part of our special day."</div>
                 </div>
               </div>
              </div>
@@ -90,7 +88,7 @@ const WeddingTimeLine=()=>{
                 <div className="spacing text-right">
                 <div className="event">Marriage</div>
                 <div className="timing mb-4">STARTS AT 10:00 AM</div>
-                <div className="Quote text-center text-md-right">"Its our pleasure to invite you all to be part of our special day"</div>
+                <div className="Quote text-center text-md-right">"We would eagerly await your kind presence at the wedding celebration."</div>
                 </div>
               </div>
              </div>
@@ -115,13 +113,14 @@ const WeddingTimeLine=()=>{
              <div className="col-6 d-flex align-items-center justify-content-center" style={{borderRight:'1px solid #ce3e7a'}}>
               <div className="caricature-box d-flex align-items-center justify-content-center">
                 <img src={image.couples}  alt="Couples"/>
+                
               </div>
              </div>
              <div className="col-6 d-flex align-items-center justify-content-center " style={{borderLeft:'1px solid #ce3e7a',width:'100%'}}>
               <div className="content-box d-flex align-items-center justify-content-center  ">
                 <div className="spacing">
                 <div className="event">Venue</div>
-                <div className="place mb-4">KBS SREE CONVENTION CENTRE</div>
+                <div className="place mb-4">KBS SREE CONVENTION CENTRE,</div>
                 <div className="address text-center text-md-left">ECR Road (Near Shivaji Statue),Puducherry-605008</div>
                 </div>
               </div>
@@ -132,6 +131,7 @@ const WeddingTimeLine=()=>{
            <div className="col-12 text-center">
               <img src={image.handHeart} width={'50px'} alt="Heart"/>
             </div>
+        </div>
         </div>
       </>
     )
