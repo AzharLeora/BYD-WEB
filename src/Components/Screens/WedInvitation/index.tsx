2809ce03c7cdbf4a516c65104f0b091d40e04027
siva2060 <cjsmart20602@gmail.com>
2024-02-02 16:01:23 +0530

    change background

--- src/Components/Screens/WedInvitation/index.tsx
+++ src/Components/Screens/WedInvitation/index.tsx
@@ -1,15 +1,29 @@
 import { Countdown, WeddingFooter, WeddingHeader, WeddingTimeLine } from '@Components//MyComponents'
 import './index.css'
 import { BYDIvitation } from '@Components//MyComponents/BYDIvitation'
+import { useEffect, useState } from 'react'
+import axios, { AxiosResponse } from 'axios'
 
 const WedInvitation=()=>{
+
+
     return (
         <>
+
         <WeddingHeader/>
+        <div className="home-bg">
+
+        <div className="container">
         <Countdown/>
+        </div>
+        <div className="container">
         <BYDIvitation/>
+        </div>
         <WeddingTimeLine/>
+        <div className="container">
         <WeddingFooter/>
+        </div>
+        </div>
         </>
     )
 }
