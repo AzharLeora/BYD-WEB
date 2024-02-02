ae9552d005011540451b7c6927efca57a0689f70
siva2060 <cjsmart20602@gmail.com>
2024-02-02 11:18:39 +0530

    update wed-invitation

--- src/App.tsx
+++ src/App.tsx
@@ -33,8 +33,8 @@ function App() {
         <Route path="/BirthdayTemplates" element={<BdTemplates/>}/>
       </Routes> */}
       {/* <Wedding001/> */}
-      {/* <WedInvitation /> */}
-      <WeddingTimeLine/>
+      <WedInvitation />
+      
     </ScreenWrapper>
 
   );
