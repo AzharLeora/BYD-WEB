0709e02fda312cf81eaccf404d641aa28adcbc7f
Vishva <134578799+Vishvap24@users.noreply.github.com>
2024-02-13 16:04:43 +0530

    Revert "Develop/byd web/vishva/share"

---
+++ src/Components/MyComponents/BYDIvitation/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { image } from '@Assets';
+import "./index.css"
+
+function BYDIvitation() {
+  
+  return (
+    <>
+    <div className='d-flex justify-content-center mx-5 my-4 BYDContainer '>
+      <img className='image-fluid bodyImage' src={image.bodyInvitation}/>
+    </div>
+    </>
+  )
+}
+
+export { BYDIvitation }
